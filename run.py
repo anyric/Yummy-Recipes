f00e15a2e564b203f9851c18f4ff8b012895a699
Anyama Richard <anyamaronyango@gmail.com>
2017-11-01 20:14:30 +0300

    updating flask and static files

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,6 @@ def index():
 @app.route('/signin')
 def signin():
     return render_template("signin.html")
-
 @app.route('/signup')
 def signup():
     return render_template("signup.html")
@@ -21,7 +20,7 @@ def category():
 @app.route('/recipes')
 def recipes():
     return render_template("recipes.html")
-
+    
 if __name__ ==  '__main__':
     app.run()
     

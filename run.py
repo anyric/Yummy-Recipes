5b40f9366491f52282ee0d03beb336d44b10f725
Anyama Richard <anyamaronyango@gmail.com>
2017-11-03 23:42:14 +0300

    added port number to run.py file

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 #from utilities.modeldb import ModelDB
 #from utilities.category import Category
 #from utilities.recipe import Recipe
+import os
 from flask import Flask, render_template, redirect, url_for, request#, flash, Session
 
 app = Flask(__name__)
@@ -87,4 +88,5 @@ def recipe():
 
 if __name__ == '__main__':
     #app.debug = True
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port = port)
